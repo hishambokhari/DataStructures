64a81e02c9d5dc65fe13245d687b729c455dd682
hishambokhari <hish.b92@gmail.com>
2020-07-17 00:08:41 +0500

    insert and lookup

---
+++ DataStructures/Trees/binary.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value){
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+  insert(value){
+
+  }
+  lookup(value){
+
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
