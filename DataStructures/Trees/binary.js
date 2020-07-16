053bf8abdf72830c8ad611c9371162b48703ffc1
hishambokhari <hish.b92@gmail.com>
2020-07-17 00:36:14 +0500

    Insert

--- DataStructures/Trees/binary.js
+++ DataStructures/Trees/binary.js
@@ -11,10 +11,31 @@ class BinarySearchTree {
     this.root = null;
   }
   insert(value){
-
+    const newNode = new Node(value);
+    if (this.root === null){
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true){
+        if(value < currentNode.value) {
+          // left
+          if(!currentNode.left){
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left
+        } else {
+          // right
+          if( !currentNode.right){
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
   }
   lookup(value){
-
   }
 }
 
@@ -26,3 +47,14 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)))
+
+function traverse(node){
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null :
+  traverse(node.left);
+  tree.right = node.right === null ? null :
+  traverse(node.right);
+  return tree;
+}
+
