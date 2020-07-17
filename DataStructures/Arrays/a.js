ab54173309ab311a665eb2ac15e9beeaabb87e2b
hishambokhari <hish.b92@gmail.com>
2020-07-17 13:25:01 +0500

    rev string

--- DataStructures/Arrays/a.js
+++ DataStructures/Arrays/a.js
@@ -2,20 +2,38 @@
 // 'Hi My name is Andrei' should be:
 // 'ierdnA si eman yM iH'
 
+// function reverse(str){
+//   // check input
+//   if (!str || str.length < 2 || typeof str !== 'string'){
+//     return 'not good'
+//   } 
+//   const backward = [];
+//   const itemLength = str.length - 1;
+
+//   for(let i = itemLength;i >= 0;i--){
+//     backward.push(str[i]);
+//   } 
+//   return backward.join('');
+
+// }
+
+
+// console.log(reverse('Hi My name is Andrei'))
+
+// Practice 
+// reverse a string 'yo man how are you'
+//check for edge cases too
+
 function reverse(str){
-  // check input
-  if (!str || str.length < 2 || typeof str !== 'string'){
-    return 'not good'
-  } 
-  const backward = [];
+  if (!str || str.length < 2){
+    return 'Not possible'
+  }
+  const reverseStr = [];
   const itemLength = str.length - 1;
 
-  for(let i = itemLength;i >= 0;i--){
-    backward.push(str[i]);
-  } 
-  return backward.join('');
-
+  for(i = itemLength; i >= 0;i--){
+    reverseStr.push(str[i])
+  }
+  return reverseStr.join('')
 }
-
-
-console.log(reverse('Hi My name is Andrei'))
+console.log(reverse('yo man how are you'))
