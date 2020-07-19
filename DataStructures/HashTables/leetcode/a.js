abf8434c1eaf3272fb70a0aef955ac0515a1f4e5
hishambokhari <hish.b92@gmail.com>
2020-07-19 14:19:08 +0500

    hash tables

---
+++ DataStructures/HashTables/leetcode/a.js
@@ -0,0 +1,12 @@
+let user = {
+  age: 54,
+  name: 'hisham',
+  magic: true,
+  scream: function() {
+    console.log('ahhhhh');
+  }
+}
+
+user.spell = 'abra kadabra' // added to object
+
+console.log(user)
