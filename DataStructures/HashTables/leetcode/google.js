cf4468c16ea9a4c2ae6ed63ab964ed3ad70850d4
hishambokhari <hish.b92@gmail.com>
2020-07-19 17:05:22 +0500

    nested for loop

--- DataStructures/HashTables/leetcode/google.js
+++ DataStructures/HashTables/leetcode/google.js
@@ -7,3 +7,18 @@
 
 // Given an array = [2,3,4,5];
 //It should return undefined.
+
+//Solution
+
+function firstRecurringChar(input){
+  for(let i=0;i < input.length; i++){
+    for(let j= i+1; j < input.length; j++){
+      if(input[i] === input[j]){
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+console.log(firstRecurringChar([2,5,1,2,3,5,1,2,4]))
