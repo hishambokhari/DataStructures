3a22f25f33d59520efe86849b63e08d5961fad36
hishambokhari <hish.b92@gmail.com>
2020-07-19 16:30:05 +0500

    insert

--- DataStructures/HashTables/leetcode/a.js
+++ DataStructures/HashTables/leetcode/a.js
@@ -1,16 +1,19 @@
-let user = {
-  age: 54,
-  name: 'hisham',
-  magic: true,
-  scream: function() {
-    console.log('ahhhhh');
-  }
-}
+// let user = {
+//   age: 54,
+//   name: 'hisham',
+//   magic: true,
+//   scream: function() {
+//     console.log('ahhhhh');
+//   }
+// }
 
-user.spell = 'abra kadabra' // added to object
+// user.spell = 'abra kadabra' // added to object
 
-user.scream()
+// user.scream()
 
 
-const a = new Map() // maintains order of insertion
-const b = new Set() // stores only keys not values
+// const a = new Map() // maintains order of insertion
+// const b = new Set() // stores only keys not values
+
+// Implementing hash tables
+
