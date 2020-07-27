10513dc1d255ccbcd421c92db6531632e980babe
hishambokhari <hish.b92@gmail.com>
2020-07-27 21:57:04 +0100

    every other element

---
+++ DataStructures/Arrays/Codewars/h.js
@@ -0,0 +1,22 @@
+// Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+
+function removeEveryOther(arr){
+  let newArr = []
+
+  for(let i = 0; i < arr.length; i = i+2){
+    newArr.push(arr[i])
+  }
+  return newArr
+}
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']))
+
+// Test.describe("Basic tests",_=>{
+//   Test.assertSimilar(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+//   Test.assertSimilar(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+//   Test.assertSimilar(removeEveryOther([[1, 2]]), [[1, 2]]);
+//   Test.assertSimilar(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+//   })
