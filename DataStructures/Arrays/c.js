4d3bcd14e7c3c2cc3f24fed7f1e719497835d47e
hishambokhari <hish.b92@gmail.com>
2020-07-27 10:40:16 +0100

    pop

--- DataStructures/Arrays/c.js
+++ DataStructures/Arrays/c.js
@@ -9,11 +9,19 @@ class TestArray {
     this.length++
   }
 
+  pop(){
+    const item = this.data[this.length-1] // last element
+    delete this.data[this.length-1]
+    this.length--
+    return item
+  }
+
 
 }
 
 const arr = new TestArray();
 arr.push('zero')
 arr.push('first')
+arr.pop('first')
 
 console.log(arr)
