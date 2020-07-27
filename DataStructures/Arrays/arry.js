73889416c30fbef12abf8611cbc63cb07f700a57
hishambokhari <hish.b92@gmail.com>
2020-07-27 10:21:31 +0100

    arrays

---
+++ DataStructures/Arrays/arry.js
@@ -0,0 +1,32 @@
+class MyArray {
+  constructor() {
+    this.length = 0
+    this.data = {};
+  }
+
+  get(index){
+    return this.data[index]
+  }
+
+  push(item){
+    this.data[this.length] = item;
+    this.length++
+    return this.length
+  }
+
+  pop() {
+    const lastItem = this.data[this.length-1]
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+newArray.pop();
+
+
+console.log(newArray);
