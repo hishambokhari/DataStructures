12febaaeb41c3ab9537e13b3d5f4ffee6e830b4a
hishambokhari <hish.b92@gmail.com>
2020-07-29 11:29:14 +0100

    cd

---
+++ DataStructures/Arrays/Codewars/aa.js
@@ -0,0 +1,21 @@
+// In this Kata, you will be given 2 or more arrays of integers, both positive and negative.
+// Your task is to find the maximum product that can be formed by taking one element from each array.
+
+// Examples:
+// solve([[1, 2],[3, 4]]) = 8, given by 2 * 4
+// solve([[10,-15],[-1,-3]]) = 45, given by (-15) * (-3)
+
+function solve(arr){
+  
+}
+
+console.log(solve([1,2,3]),[1,2,3])
+
+// describe("Basic tests", function(){
+//   Test.assertEquals(solve([[1, 2],[3, 4]]),8);
+//   Test.assertEquals(solve([[10,-15],[-1,-3]]),45);
+//   Test.assertEquals(solve([[-1,2,-3,4],[1,-2,3,-4]]),12);
+//   Test.assertEquals(solve([[-11,-6],[-20,-20],[18,-4],[-20, 1]]),17600);
+//   Test.assertEquals(solve([[14,2],[0,-16],[-12,-16]]),3584);
+//   Test.assertEquals(solve([[-3,-4],[1,2,-3]]),12);
+//   });
---
+++ DataStructures/Arrays/Codewars/j.js
@@ -0,0 +1,25 @@
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+  
+}
+
+// function testing(actual, expected) {
+//   Test.assertEquals(actual, expected)
+// }
+// Test.describe("howMuchILoveYou",function() {
+//   Test.it("Basic tests",function() { 
+//       testing(howMuchILoveYou(7),"I love you")
+//       testing(howMuchILoveYou(3),"a lot")
+//       testing(howMuchILoveYou(6),"not at all")
+// })})
