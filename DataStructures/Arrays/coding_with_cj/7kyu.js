9b0e316a6bcdc83eb2d4e5b17a5ce662874e5bc5
hishambokhari <hish.b92@gmail.com>
2020-07-29 13:29:52 +0100

    7kyu

---
+++ DataStructures/Arrays/coding_with_cj/7kyu.js
@@ -0,0 +1,25 @@
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(number) {
+  let result = []
+
+  for(let i = 2; i < number; i++){
+    if (number % i == 0){
+      result.push(i);
+    } 
+    }
+    if (result.length == 0) {
+      return number + ' is prime';
+    }
+  
+  return result;
+}
+console.log(divisors(13))
+// Test.assertDeepEquals(divisors(15), [3, 5]);
+// Test.assertDeepEquals(divisors(12), [2, 3, 4, 6]);
+// Test.assertDeepEquals(divisors(13), "13 is prime");
