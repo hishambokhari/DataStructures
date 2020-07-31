1fd8c22dfc4409ded19e4677d9fa4a0a120290f9
hishambokhari <hish.b92@gmail.com>
2020-07-31 12:06:42 +0100

    max diff

--- DataStructures/Arrays/coding_with_cj/ho.js
+++ DataStructures/Arrays/coding_with_cj/ho.js
@@ -16,13 +16,47 @@
 
 //q2
 
-function squareSum(numbers){
-  let sum = [];
+// function squareSum(numbers){
+//   let sum = [];
   
-  for(let i =0; i < numbers.length; i++){
-    sum.push(numbers[i]**2)
-  }
-  return sum.reduce(function(a,b){return a+b},0)
-}
+//   for(let i =0; i < numbers.length; i++){
+//     sum.push(numbers[i]**2)
+//   }
+//   return sum.reduce(function(a,b){return a+b},0)
+// }
 
-console.log(squareSum([0,3,4,5]))
+// console.log(squareSum([0,3,4,5]))
+
+//q3
+
+// You must implement a function maxDiff that return the difference between the biggest and the smallest value in a list(lst) received as parameter.
+
+// The list(lst) contains integers, that means it may contain some negative numbers.
+
+// If the list is empty or contains a single element, return 0.
+
+// The list(lst) is not sorted.
+
+function maxDiff(list) {
+
+  if(list == [] || list < 2){
+    return 0
+  }
+  
+    let sortedList = list.sort(function(a,b){return a-b})
+    let firstVal = sortedList[0]
+    let lastVal = sortedList[list.length -1]
+    let maximumDiff = lastVal - firstVal
+    return maximumDiff
+
+};
+console.log(maxDiff([16]))
+// Test.describe('Basic tests', function() {
+
+//   Test.assertEquals(maxDiff([0, 1, 2, 3, 4, 5, 6]), 6);
+//   Test.assertEquals(maxDiff([-0, 1, 2, -3, 4, 5, -6]), 11);
+//   Test.assertEquals(maxDiff([0, 1, 2, 3, 4, 5, 16]), 16);
+//   Test.assertEquals(maxDiff([16]), 0);
+//   Test.assertEquals(maxDiff([]), 0);
+
+// });
