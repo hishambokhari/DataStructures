08308d8227b0605b9c030f0884c974bb9293ae9e
hishambokhari <hish.b92@gmail.com>
2020-07-31 21:58:31 +0100

    result

--- DataStructures/Arrays/coding_with_cj/level_7/e.js
+++ DataStructures/Arrays/coding_with_cj/level_7/e.js
@@ -1,13 +1,35 @@
-var stringToNumber = function(str){
-  return parseFloat(str)
-}
-console.log(stringToNumber("1234"))
+// // var stringToNumber = function(str){
+// //   return parseFloat(str)
+// // }
+// // console.log(stringToNumber("1234"))
 
-// describe( "stringToNumber", function(){
-//   it( "should work for the examples" , function(){
-//     Test.assertEquals(stringToNumber("1234"),1234)
-//     Test.assertEquals(stringToNumber("605"), 605)
-//     Test.assertEquals(stringToNumber("1405"),1405)
-//     Test.assertEquals(stringToNumber("-7"),  -7)
-//   });
-// });
+// // describe( "stringToNumber", function(){
+// //   it( "should work for the examples" , function(){
+// //     Test.assertEquals(stringToNumber("1234"),1234)
+// //     Test.assertEquals(stringToNumber("605"), 605)
+// //     Test.assertEquals(stringToNumber("1405"),1405)
+// //     Test.assertEquals(stringToNumber("-7"),  -7)
+// //   });
+// // });
+
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+function add(a, b){
+  if (a.toString() === b || a === b.toString()){
+    return true
+  } else {
+    return false
+  }
+  
+}
+console.log(add(1,'1'))
+
+
+// Test.assertEquals(add('1', 1), true);
+// Test.assertEquals(add(1, '1'), true);
+// Test.assertEquals(add(1, '0'), false);
+// Test.assertEquals(add('11', 11), true);
+// Test.assertEquals(add(12, 12), true);
+// Test.assertEquals(add(120, '021'), false);
