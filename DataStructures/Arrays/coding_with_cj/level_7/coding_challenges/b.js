ef40a240da4074701f5399cd7f79cede1b3fcd26
hishambokhari <hish.b92@gmail.com>
2020-08-02 20:40:47 +0100

    fib

--- DataStructures/Arrays/coding_with_cj/level_7/coding_challenges/b.js
+++ DataStructures/Arrays/coding_with_cj/level_7/coding_challenges/b.js
@@ -1,9 +1,19 @@
 //Recursion
 
-function factorial(n){
-  if (n == 1 || n == 0){
-    return 1;
+// function factorial(n){
+//   if (n == 1 || n == 0){
+//     return 1;
+//   }
+//   return n * factorial(n-1)
+// }
+// console.log(factorial(2))
+
+//Fibonaci
+
+function fibb(num){
+  if(num <= 2){
+    return num;
   }
-  return n * factorial(n-1)
+  return fibb(num-1) + fibb(num-2)
 }
-console.log(factorial(2))
+console.log(fibb(5))
